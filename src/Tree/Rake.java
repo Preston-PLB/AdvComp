0a6f26cee2f389fdb494083639fa7ed9079716f3
Preston Baxter <baxterboyz1@gmail.com>
2018-02-02 16:25:22 -0600

    Wrote some crappy code
    Signed-off-by: Preston Baxter <baxterboyz1@gmail.com>

--- src/Tree/Rake.java
+++ src/Tree/Rake.java
@@ -1,7 +1,62 @@
 package Tree;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * Created by 131111 on 1/31/2018.
  */
 public class Rake {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("rake.dat"));
+
+        while(s.hasNextLine()){
+            String line = s.nextLine();
+            if(line.equals("-1")){
+                return;
+            }
+
+            HashMap<Integer, Integer> columns = new HashMap<>();
+
+            char[] chars = line.replaceAll("-1", "-").replaceAll(" ", "").toCharArray();
+
+            int column = 0;
+            boolean left = true;
+
+            for(char c: chars){
+                if(c == '-'){
+                    if(!left){
+                        column++;
+                        left = true;
+                    }else{
+                        left = false;
+                    }
+                }else{
+                    if(left){
+                        if(!columns.containsKey(column)){
+                            columns.put(column, 1);
+                        }else{
+                            columns.put(column, columns.get(column));
+                        }
+                        column -= 1;
+                    }else{
+                        if(!columns.containsKey(column)){
+                            columns.put(column, 1);
+                        }else{
+                            columns.put(column, columns.get(column));
+                        }
+                        column += 1;
+                    }
+                }
+            }
+            for(Integer i: columns.values()){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
 }
