d91f719be06ed6b02cf95169bb4e34749e91fd49
Preston Baxter <baxterboyz1@gmail.com>
2018-02-12 16:25:12 -0600

    More redMeat (NSFJoM)
    Signed-off-by: Preston Baxter <baxterboyz1@gmail.com>

--- src/Tree/Heap/RedMeat.java
+++ src/Tree/Heap/RedMeat.java
@@ -1,4 +1,13 @@
 package Tree.Heap;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class RedMeat {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("goat.dat"));
+    }
+
 }
