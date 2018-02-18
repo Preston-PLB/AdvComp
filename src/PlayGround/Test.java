cd49818f460bc5558d298faaa8b26e2e09d68e85
Preston-PLB <baxterboyz1@gmail.com>
2018-02-17 19:45:11 -0600

    Tested BitInputStream
    Signed-off-by: Preston-PLB <baxterboyz1@gmail.com>

--- src/PlayGround/Test.java
+++ src/PlayGround/Test.java
@@ -1,22 +1,26 @@
 package PlayGround;
 
+import Compressor.BitInputStream;
 import Hash.HashTableLL;
 import Hash.Name;
 import LinkedLists.LinkedListStack;
 import Tree.Heap.Heap;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
 
 public class Test {
 
     public static void main(String[] args){
-        Heap heap = new Heap(5);
-        heap.insert(5);
-        heap.insert(2);
-        heap.insert(6);
-        heap.insert(22);
-        heap.insert(4);
+        BitInputStream bs = new BitInputStream(new File("compressionTest.txt"));
 
-        System.out.println(heap);
+        for(int i = 0; i<100; i++){
+            try {
+                System.out.print((char)bs.read());
+            }catch (IOException ie){
+                ie.printStackTrace();
+            }
+        }
     }
 }
