1e4f787fa2fbc3e136670b277b0b95d499ae9a1b
Preston-PLB <baxterboyz1@gmail.com>
2018-03-03 10:26:16 -0600

    Updated Graph interface and implemented Graph in UnorderedGraph
    Signed-off-by: Preston-PLB <baxterboyz1@gmail.com>

--- src/GraphTheory/Graph.java
+++ src/GraphTheory/Graph.java
@@ -1,4 +1,9 @@
 package GraphTheory;
 
 public interface Graph {
+
+    int vertices();
+    int edges();
+    Iterable<Integer> getConnections(int vertex);
+
 }
--- src/GraphTheory/UnorderedGraph.java
+++ src/GraphTheory/UnorderedGraph.java
@@ -5,43 +5,68 @@ import java.util.LinkedList;
 /**
  * Created by 131111 on 3/1/2018.
  */
-public class UnorderedGraph {
+public class UnorderedGraph implements Graph {
 
-    private int size;
-    private LinkedList<Integer> larry[];
+    private int size, edges;
+    private LinkedList<Integer> vertices[];
+    private boolean connect;
 
     @SuppressWarnings("unchecked")
     public UnorderedGraph(){
-        larry = new LinkedList[10];
+        vertices = new LinkedList[10];
 
         for(int i = 0; i<10 ;i++){
-            larry[i] = new LinkedList<>();
+            vertices[i] = new LinkedList<>();
         }
     }
 
     @SuppressWarnings("unchecked")
     public UnorderedGraph(int vertices){
-        larry = new LinkedList[vertices];
+        this.vertices = new LinkedList[vertices];
         size = vertices;
+        edges = 0;
 
         for(int i = 0; i<vertices ;i++){
-            larry[i] = new LinkedList<>();
+            this.vertices[i] = new LinkedList<>();
         }
     }
 
     public void addEdge(int src, int dest){
-        larry[src].addFirst(dest);
-        larry[dest].addFirst(src);
+        vertices[src].addFirst(dest);
+        vertices[dest].addFirst(src);
+        edges++;
     }
 
-    public boolean connects(int src, int dest, int index){
-        if(larry[src].get(index) == dest){
-            return true;
+    @Override
+    public int vertices() {
+        return size;
+    }
+
+    @Override
+    public int edges() {
+        return 0;
+    }
+
+    @Override
+    public Iterable<Integer> getConnections(int vertex) {
+        return vertices[vertex];
+    }
+
+    public boolean connects(int src, int dest){
+        connect = false;
+        path(src, dest, 0, new boolean[size]);
+        return connect;
+    }
+
+    private void path(int src, int dest, int index, boolean[] visited){
+        if(vertices[src].get(index) == dest){
+            connect = true;
         }else{
-            if(index+1 == larry[src].size()){
-                return connects(larry[src].get(index), dest, 0);
-            }else{
-                return connects(src, dest, index+1);
+            visited[src] = true;
+            if(!visited[vertices[src].get(index)]){
+                path(vertices[src].get(index), dest, 0, visited);
+            }else if(index+1 != vertices[src].size() && !visited[vertices[src].get(index+1)]){
+                path(src, dest, index+1, visited);
             }
         }
     }
