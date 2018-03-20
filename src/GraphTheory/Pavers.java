39f102836a8c6ccd1ffcf19431efd333f9a151a3
Preston-PLB <baxterboyz1@gmail.com>
2018-03-19 22:18:49 -0500

    Wrote code for pavers
    Signed-off-by: Preston-PLB <baxterboyz1@gmail.com>

---
+++ src/GraphTheory/Pavers.java
@@ -0,0 +1,45 @@
+package GraphTheory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Pavers {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("pavers.dat"));
+
+        int tc = s.nextInt();
+
+
+        for(int x = 0; x<tc; x++){
+            int price = s.nextInt();
+            int buildings = s.nextInt();
+            int streets = s.nextInt();
+
+            WeightedGraph map = new WeightedGraph(buildings);
+
+            for(int i = 0; i<streets; i++){
+                int a = s.nextInt();
+                int b = s.nextInt();
+                int c = s.nextInt();
+
+                map.addEdge(a,b,c);
+            }
+
+            MinimumSpanningTree mst = new MinimumSpanningTree(map);
+            WeightedGraph optimizedMap = mst.getMst();
+
+            int cost = 0;
+
+            for(int i = 0; i<optimizedMap.vertices(); i++){
+                Iterable<Integer> list = optimizedMap.getWeights(i);
+                for(Integer l: list){
+                    cost+=l*price;
+                }
+            }
+            System.out.println(cost);
+        }
+    }
+
+}
