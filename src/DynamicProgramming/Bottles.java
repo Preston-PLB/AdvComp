beea108b1a0561aaf6b83551226d39c18048326a
Preston-PLB <baxterboyz1@gmail.com>
2018-04-11 19:28:58 -0500

    Finished Bottles
    Signed-off-by: Preston-PLB <baxterboyz1@gmail.com>

---
+++ src/DynamicProgramming/Bottles.java
@@ -0,0 +1,48 @@
+package DynamicProgramming;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bottles {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("bottles.dat"));
+
+        int tc = s.nextInt();
+
+        for(int x = 0; x<tc; x++){
+            int b = s.nextInt();
+            int[] vol = new int[b];
+
+            for(int i = 0; i<b; i++){
+                vol[i] = s.nextInt();
+            }
+
+            System.out.println(maxVol(vol));
+        }
+    }
+
+    private static int maxVol(int[] vol) {
+        int[] dp = new int[vol.length];
+        Arrays.fill(dp, -1);
+        return loop(vol, dp, vol.length-1);
+    }
+
+    private static int loop(int[] vol, int[] dp, int i) {
+        if(i == 0){
+            dp[0] = vol[0];
+            return dp[0];
+        }else if(i == 1){
+            dp[1] = Math.max(vol[0], vol[1]);
+            return dp[1];
+        }else{
+            if(dp[i] == -1){
+                dp[i] = Math.max(loop(vol, dp, i-2)+vol[i], loop(vol, dp, i-1));
+            }
+            return dp[i];
+        }
+    }
+
+}
