207a9775a59522ce0451dc3c099f6378c5ee516d
Preston-PLB <baxterboyz1@gmail.com>
2018-04-30 20:28:25 -0500

    Implemeted memoized recursion
    Signed-off-by: Preston-PLB <baxterboyz1@gmail.com>

--- src/DynamicProgramming/CasinoRoyal.java
+++ src/DynamicProgramming/CasinoRoyal.java
@@ -13,17 +13,48 @@ public class CasinoRoyal {
         int tc = s.nextInt();
         for(int x = 0; x<tc; x++){
             int lines = s.nextInt();
+            s.nextLine();
+            PachinkoTree pt = new PachinkoTree(lines);
             for(int y = 1; y<=lines; y++){
-                String[] vals = s.nextLine().split(" ");
+                String[] vals = s.nextLine().trim().split(" ");
+                for(String str: vals){
+                    pt.insert(Integer.parseInt(str));
+                }
             }
+
+            System.out.println(getPath(pt));
+
         }
+    }
+
+    public static int getPath(PachinkoTree pt){
+        int[][] dp = new int[pt.getDepth()][pt.getDepth()];
+        for(int[] i: dp){
+            Arrays.fill(i, -1);
+        }
+        return recursive(pt, 0,0, dp);
+    }
+
+    private static int recursive(PachinkoTree pt, int row, int index, int[][] dp){
+        if(pt.isLeaf(row)){
+            dp[row][index] = pt.getValue(row,index);
+            return dp[row][index];
+        }else{
+            if(dp[row][index] == -1){
+                int val = pt.getValue(row, index);
+                dp[row][index] = Math.max(recursive(pt, row+1, index, dp)+val, recursive(pt, row+1, index+1, dp)+val);
+            }
+            return dp[row][index];
+        }
+
+
     }
 
 }
 
 class PachinkoTree{
 
-    int[][] tree;
+    private int[][] tree;
 
     public PachinkoTree(int depth){
         tree = new int[depth][depth];
@@ -47,12 +78,19 @@ class PachinkoTree{
         }
     }
 
-    public int getLeftChild(int row, int index){
-        return tree[row+1][index];
+    public int getValue(int row, int index){
+        if(row > tree.length || index > tree.length){
+            return -1;
+        }
+        return tree[row][index];
     }
 
-    public int getRightChild(int row, int index){
-        return tree[row+1][index+1];
+    public int getDepth(){
+        return tree.length;
+    }
+
+    public boolean isLeaf(int row){
+        return row >= tree.length-1;
     }
 
 }
