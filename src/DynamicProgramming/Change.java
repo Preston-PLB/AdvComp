304ea42f143732e54ecf22f29355a03c8aaa7a73
Preston Baxter <baxterboyz1@gmail.com>
2018-05-09 15:44:25 -0500

    Init change problem
    Signed-off-by: Preston Baxter <baxterboyz1@gmail.com>

---
+++ src/DynamicProgramming/Change.java
@@ -0,0 +1,13 @@
+package DynamicProgramming;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Change {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("change.dat"));
+    }
+
+}
