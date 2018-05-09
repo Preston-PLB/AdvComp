e30c593f70099b697e360fd797f1d8d31c2669db
Preston Baxter <baxterboyz1@gmail.com>
2018-05-09 15:41:42 -0500

    Cleaned and made readable
    Signed-off-by: Preston Baxter <baxterboyz1@gmail.com>

--- src/DynamicProgramming/Truck.java
+++ src/DynamicProgramming/Truck.java
@@ -14,19 +14,19 @@ public class Truck {
         for(int h = 0; h<tc; h++){
             int cap = s.nextInt();
             int pairs = s.nextInt();
-            int[] space = new int[pairs];
-            int[] cost = new int[pairs];
+            int[] weights = new int[pairs];
+            int[] values = new int[pairs];
 
             for(int i = 0; i<pairs; i++){
-                space[i] = s.nextInt();
-                cost[i] = s.nextInt();
+                weights[i] = s.nextInt();
+                values[i] = s.nextInt();
             }
 
-            System.out.println(getMax(cap, pairs, space, cost));
+            System.out.println(getMax(cap, pairs, weights, values));
         }
     }
 
-    private static int getMax(int cap, int pairs, int[] space, int[] cost) {
+    private static int getMax(int cap, int pairs, int[] weights, int[] values) {
         int[][] dp = new int[pairs+1][cap+1];
         for(int[] array: dp){
             Arrays.fill(array, -1);
@@ -34,17 +34,18 @@ public class Truck {
         }
         Arrays.fill(dp[0], 0);
 
-        return recursive(dp, space, cost, dp[0].length-1, dp.length-1);
+        return recursive(dp, weights, values, dp[0].length-1, dp.length-1);
+        //return dynamic(dp, weights, values);
     }
 
-    private static int dynamic(int[][] dp, int[] weights, int[] value){
+    private static int dynamic(int[][] dp, int[] weights, int[] values){
         for(int y = 1; y<dp.length; y++){
             for(int x = 1; x<dp[0].length; x++){
                 if(x < weights[y-1]){
                     dp[y][x] = Math.max(0, dp[y-1][x]);
                 }else{
                     int ly = y-1;
-                    int choice1 = dp[y][x-weights[ly]]+value[ly];
+                    int choice1 = dp[y][x-weights[ly]]+values[ly];
                     int choice2 = dp[ly][x];
                     dp[y][x] = Math.max(choice1, choice2);
                 }
@@ -53,24 +54,20 @@ public class Truck {
         return dp[dp.length-1][dp[0].length-1];
     }
 
-    private static int recursive(int[][] dp, int[] weights, int[] value, int i, int w){
+    private static int recursive(int[][] dp, int[] weights, int[] values, int i, int w){
         if(w < 1){
             return 0;
         }
         if(dp[w][i] == -1){
             if(i < weights[w-1]){
-                dp[w][i] = Math.max(0, recursive(dp, weights, value, i, w-1));
+                dp[w][i] = Math.max(0, recursive(dp, weights, values, i, w-1));
             }else{
-                dp[w][i] = Math.max(recursive(dp, weights, value, i-weights[w-1], w)+value[w-1], recursive(dp, weights, value, i, w-1));
+                int lw = w-1;
+                int choice1 = recursive(dp, weights, values, i-weights[lw], w)+values[lw];
+                int choice2 = recursive(dp, weights, values, i, lw);
+                dp[w][i] = Math.max(choice1, choice2);
             }
         }
         return dp[w][i];
-    }
-
-    private static void printArray(int[][] array){
-        for(int[] a: array){
-            System.out.println(Arrays.toString(a));
-        }
-        System.out.println();
     }
 }
