730e0a267698fd8b7e4d76c02fd26c10d65329c2
Preston Baxter <baxterboyz1@gmail.com>
2018-05-21 16:10:24 -0500

    Got DP working
    Signed-off-by: Preston Baxter <baxterboyz1@gmail.com>

---
+++ src/DynamicProgramming/BottlesCoolaid.java
@@ -0,0 +1,61 @@
+package DynamicProgramming;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BottlesCoolaid {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("bottles.dat"));
+
+        int tc = s.nextInt();
+
+        for(int x = 0; x<tc; x++){
+            int b = s.nextInt();
+            int[] vol = new int[b];
+
+            for(int i = 0; i<b; i++){
+                vol[i] = s.nextInt();
+            }
+
+            System.out.println(maxVol(vol));
+        }
+    }
+
+    private static int maxVol(int[] vol) {
+        int[] dp = new int[vol.length];
+        Arrays.fill(dp, -1);
+        return loop(vol,dp, vol.length);
+    }
+
+    private static int loop(int[] vol, int[] dp, int i) {
+        if(i == 0){
+            dp[0] = vol[0];
+            return dp[0];
+        }else if(i == 1){
+            dp[1] = dp[0] < dp[1] ? vol[0]+vol[1] : Math.max(vol[0], vol[1]);
+            return dp[1];
+        }else{
+            if(dp[i] == -1){
+                dp[i] = dp[i-1] < dp[i] ?  Math.max(loop(vol, dp, i-2)+vol[i], loop(vol, dp, i-1)+vol[i]) : Math.max(loop(vol, dp, i-2)+vol[i], loop(vol, dp, i-1));
+            }
+            return dp[i];
+        }
+    }
+
+    private static int othetrLoop(int[] vol, int[] dp){
+        dp[0] = vol[0];
+        dp[1] = dp[1] > dp[0] ? dp[1]+dp[0] : Math.max(vol[0], vol[1]);
+        for(int i = 2; i<vol.length; i++){
+            if(dp[i] > dp[i-1]){
+                dp[i] = Math.max(dp[i-2]+vol[i], dp[i-1]+vol[i]);
+            }else{
+                dp[i] = Math.max(dp[i - 2] + vol[i], dp[i - 1]);
+            }
+        }
+        return dp[dp.length-1];
+    }
+
+}
--- src/DynamicProgramming/Change.java
+++ src/DynamicProgramming/Change.java
@@ -17,7 +17,7 @@ public class Change {
 
             int[] coins = new int[numberOfCoins];
 
-            for(int y = 0; y<x; y++){
+            for(int y = 0; y<numberOfCoins; y++){
                 coins[y] = s.nextInt();
             }
 
@@ -26,7 +26,7 @@ public class Change {
     }
 
     private static int getChange(int change, int[] coins) {
-        int[][] dp = new int[coins.length][change];
+        int[][] dp = new int[coins.length+1][change];
         for(int[] array: dp){
             Arrays.fill(array, Integer.MAX_VALUE);
         }
@@ -34,11 +34,33 @@ public class Change {
     }
 
     private static int dynamic(int[][] dp, int[] coins) {
-        for(int y = 0; y<dp.length; y++){
-            for(int x = 0; x<dp[0].length; x++){
 
+        for(int i = 0; i<dp[0].length; i++){
+            dp[1][i] = i % coins[0] == 0 ? i / coins[0] : Integer.MAX_VALUE;
+        }
+
+        for(int y = 2; y<dp.length; y++){
+            for(int x = 0; x<dp[0].length; x++){
+                if(x < coins[y-1]){
+                    dp[y][x] = dp[y-1][x];
+                }else{
+                    int choice1 = dp[y][x-coins[y-1]]+1;
+                    int choice2 = dp[y-1][x];
+                    dp[y][x] = Math.min(choice1, choice2);
+                }
             }
         }
-        return 0;
+        printarray(dp);
+        return dp[dp.length-1][dp[0].length-1];
     }
+
+
+
+
+    static void printarray(int[][] arr){
+        for(int[] a: arr){
+            System.out.println(Arrays.toString(a));
+        }
+    }
+
 }
